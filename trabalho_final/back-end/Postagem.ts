06b17089fe0006ac479a453e65b412d4ca2320c3
meir & teresa <meirnaa666@gmail.com>
2025-07-21 11:52:35 -0300

    update para denuncias, reações e compartilhar

--- trabalho_final/back-end/Postagem.ts
+++ trabalho_final/back-end/Postagem.ts
@@ -5,14 +5,23 @@ export class Postagem {
     private data: Date;
     private curtidas: number;
     private comentarios: Comentario[];
+    private reacoes: { [emoji: string]: number } = {
+            Risos: 0,
+            Surpresa: 0,
+            Raiva: 0,
+            Choro: 0
+    };
+    private denuncias: Denuncia[];
 
-    constructor(id: number, titulo: string, conteudo: string, data: Date, curtidas: number) {
+    constructor(id: number, titulo: string, conteudo: string, data: Date, curtidas: number, reacoes: { [emoji: string]: number } = {}) {
         this.id = id;
         this.titulo = titulo;
         this.conteudo = conteudo;
         this.data = data;
         this.curtidas = curtidas;
         this.comentarios = [];
+        this.reacoes = reacoes;
+        this.denuncias = [];
     }
 
     public getComentarios(): Comentario[] {
@@ -21,6 +30,22 @@ export class Postagem {
 
     public adicionarComentario(autor: string, texto: string, data?: Date): void {
         this.comentarios.push(new Comentario(autor, texto, data ?? new Date()));
+    }
+
+    public reagir(emoji: string): void {
+        this.reacoes[emoji] = (this.reacoes[emoji] || 0) + 1;
+    }
+
+    public getDenuncias(): Denuncia[] {
+        return this.denuncias;
+    }
+
+    public adicionarDenuncia(autor: string, texto: string, data?: Date): void {
+        this.denuncias.push(new Denuncia(autor, texto, data ?? new Date()));
+    }
+
+    public getReacoes(): { [emoji: string]: number } {
+        return this.reacoes;
     }
 
     public getId(): number {
@@ -45,6 +70,30 @@ export class Postagem {
 }
 
 export class Comentario {
+    private autor: string;
+    private texto: string;
+    private data: Date;
+
+    constructor(autor: string, texto: string, data: Date) {
+        this.autor = autor;
+        this.texto = texto;
+        this.data = data;
+    }
+
+    public getAutor(): string {
+        return this.autor;
+    }
+
+    public getTexto(): string {
+        return this.texto;
+    }
+
+    public getData(): Date {
+        return this.data;
+    }
+}
+
+export class Denuncia {
     private autor: string;
     private texto: string;
     private data: Date;
